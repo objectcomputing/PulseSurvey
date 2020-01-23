50b7453ab411cb7783504cd38be77f4ee6e5082b
Holly Williams <williamsh@objectcomputing.com>
2020-01-23 12:15:43 -0600

    added test for response and fixed response repo to reflect db

--- send-surveys/src/main/java/com/objectcomputing/pulsesurvey/receive/responses/SurveyResponseController.java
+++ send-surveys/src/main/java/com/objectcomputing/pulsesurvey/receive/responses/SurveyResponseController.java
@@ -25,5 +25,6 @@ public class SurveyResponseController {
                                " with a key of: " + surveyKey);
 
     }
+    
 
 }
--- send-surveys/src/main/java/com/objectcomputing/pulsesurvey/repositories/ResponseRepository.java
+++ send-surveys/src/main/java/com/objectcomputing/pulsesurvey/repositories/ResponseRepository.java
@@ -1,7 +1,6 @@
 package com.objectcomputing.pulsesurvey.repositories;
 
 import com.objectcomputing.pulsesurvey.model.Response;
-import com.objectcomputing.pulsesurvey.model.ResponseKey;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
@@ -10,10 +9,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
-
-import io.micronaut.data.jdbc.annotation.JdbcRepository;
-import io.micronaut.data.model.query.builder.sql.Dialect;
-import io.micronaut.data.repository.CrudRepository;
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 public interface ResponseRepository extends CrudRepository<Response, UUID> {
